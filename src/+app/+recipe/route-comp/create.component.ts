ec7afea599c13d345caeda856a9ca6870cc12ab4
khex <o.kholjavko@yandex.ua>
2016-12-19 00:45:36 +0200

    post list

--- src/+app/+recipe/route-comp/create.component.ts
+++ src/+app/+recipe/route-comp/create.component.ts
@@ -6,6 +6,6 @@ import { Component,
   changeDetection: ChangeDetectionStrategy.Default,
   encapsulation: ViewEncapsulation.Emulated,
   selector: 'create',
-  template: `<h3>Post Create Component</h3>`
+  template: `<h3>Create Post Component</h3>`
 })
 export class CreateComponent { }
--- src/+app/+recipe/route-comp/read.component.ts
+++ src/+app/+recipe/route-comp/read.component.ts
@@ -6,6 +6,16 @@ import { Component,
   changeDetection: ChangeDetectionStrategy.Default,
   encapsulation: ViewEncapsulation.Emulated,
   selector: 'read',
-  template: `<h3>Post Read Component</h3>`
+  template: `
+  <h3>Post Read Component</h3>
+  <li><a routerLinkActive="router-link-active"
+         routerLink="/post/add">Add Post</a></li>
+  <li><a routerLinkActive="router-link-active"
+         routerLink="/post/1715">Read By ID</a></li>
+  <li><a routerLinkActive="router-link-active"
+         routerLink="/post/1715/delete">Delete By ID</a></li>
+  <li><a routerLinkActive="router-link-active"
+         routerLink="/post/1715/update">Update By ID</a></li>
+  `
 })
 export class ReadComponent { }
