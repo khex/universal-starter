b2dc92dfd7780a9160459a637799b1374e933bc8
khex <o.kholjavko@yandex.ua>
2017-04-30 00:48:38 +0300

    bugfix
    This reverts commit d542a50fdea96db93c0c1194d8a448526a7062bc.

--- src/+app/+recipe/recipe.module.ts
+++ src/+app/+recipe/recipe.module.ts
@@ -1,30 +1,29 @@
-import { NgModule }            from '@angular/core';
-import { FormsModule,
-         ReactiveFormsModule } from '@angular/forms';
-import { BrowserModule,
-         Title }               from '@angular/platform-browser';
-import { SelectModule }        from 'ng2-select/ng2-select';
-import { SharedModule }        from '../shared/shared.module';
-import { Routing }             from './recipe-routing.module';
-import { CreateComponent }     from './create-update/create.component';
-import { ReadOneComponent }    from './read-one/read-one.component';
-import { ReadManyComponent }   from './read-many/read-many.component';
-import { UpdateComponent }     from './create-update/update.component';
-import { Meta }                from '../../angular2-meta';
-
-
-@NgModule({
-  imports:      [ Routing,
-                  SharedModule,
-                  FormsModule,
-                  SelectModule,
-                  ReactiveFormsModule,
-                  BrowserModule ],
-  declarations: [ ReadManyComponent,
-                  CreateComponent,
-                  ReadOneComponent,
-                  UpdateComponent ],
-  providers:    [ Meta,
-                  Title ]
-})
-export class RecipeModule { }
+import { NgModule }            from '@angular/core';
+import { FormsModule,
+         ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule,
+         Title }               from '@angular/platform-browser';
+import { SelectModule }        from 'ng2-select/ng2-select';
+import { SharedModule }        from '../shared/shared.module';
+import { Routing }             from './recipe-routing.module';
+import { CreateComponent }     from './create-update/create.component';
+import { ReadOneComponent }    from './read-one/read-one.component';
+import { ReadManyComponent }   from './read-many/read-many.component';
+import { UpdateComponent }     from './create-update/update.component';
+import { Meta }                from '../../angular2-meta';
+
+@NgModule({
+  imports:      [ Routing,
+                  SharedModule,
+                  FormsModule,
+                  SelectModule,
+                  ReactiveFormsModule,
+                  BrowserModule ],
+  declarations: [ ReadManyComponent,
+                  CreateComponent,
+                  ReadOneComponent,
+                  UpdateComponent ],
+  providers:    [ Meta,
+                  Title ]
+})
+export class RecipeModule { }
